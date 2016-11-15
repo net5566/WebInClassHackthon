3b72a8b7359ca39afabc0b2ea15d037af2cb7c68
Net Tseng <b02901184@ntu.edu.tw>
2016-11-16 02:23:43 +0800

    bad.html is not working QAQ

--- server.js
+++ server.js
@@ -22,8 +22,18 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use('/api', api);
 
 app.get('*', (req, res) => {
+ // console.log('getin')
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.use((req, res) => {
+ // console.log('404');
+  res.status(404)
+     .sendFile(path.join(__dirname, 'bad.html'));
+});
+
+// i have no idea that why it can get into bad.html 
+// QAQ
 
 app.listen(port, err => {
   if (err) {
